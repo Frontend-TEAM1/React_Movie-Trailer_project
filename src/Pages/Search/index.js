c27056f3d394ae442d2c5a1602f6f09e58df390d
JAE HOON LEE <jazzyrain722@naver.com>
2023-03-24 16:51:28 +0900

    스크롤링4

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -1,5 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchSearchResults } from 'Apis/searchApi';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import MovieApi from 'Apis/movieApi';
+import { useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import SearchedMovies from './components/searchedMovies';
@@ -11,19 +12,55 @@ function SearchPage() {
 	const word = searchParams.get('word');
 
 	const {
-		isLoading,
-		isError,
 		data: searchResults,
-		error,
-	} = useQuery(['searchResults', word], () => fetchSearchResults(word));
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useInfiniteQuery(
+		['searchResults', word],
+		({ pageParam = 1 }) => MovieApi.getSearch(word, pageParam),
+		{
+			onError: () => console.log('에러'),
+			getNextPageParam: (lastPage, allPages) => {
+				if (lastPage.data.page < lastPage.data.total_pages) {
+					return lastPage.data.page + 1;
+				} else {
+					return undefined;
+				}
+			},
+		},
+	);
+	const handleScroll = useCallback(
+		node => {
+			if (node && !isFetchingNextPage && hasNextPage) {
+				const observer = new IntersectionObserver(
+					entries => {
+						if (entries[0].isIntersecting) {
+							fetchNextPage();
+						}
+					},
+					{
+						rootMargin: '0px 0px 500px 0px',
+					},
+				);
+				observer.observe(node);
+			}
+		},
+		[hasNextPage, fetchNextPage],
+	);
 
 	return (
 		<Wrapper>
 			<h1>Search "{word}"</h1>
 			{searchResults &&
-				searchResults.map(movie => (
-					<SearchedMovies key={movie.id} movie={movie}></SearchedMovies>
-				))}
+				searchResults.pages.map(
+					page =>
+						page &&
+						page.data.results.map(movie => (
+							<SearchedMovies key={movie.id} movie={movie} />
+						)),
+				)}
+			<div ref={handleScroll}></div>
 		</Wrapper>
 	);
 }
